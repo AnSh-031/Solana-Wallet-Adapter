868e9aa51a3430bb9d13b8755fd37d76981f20c4
AnSh-031 <bhardwajansh0301@gmail.com>
2025-12-05 00:44:33 +0530

    transaction issue fixed

--- src/SignMsg.jsx
+++ src/SignMsg.jsx
@@ -1,9 +1,12 @@
 import { useWallet } from "@solana/wallet-adapter-react";
 import { ed25519 } from "@noble/curves/ed25519.js"
 import bs58 from "bs58"
+import { useState } from "react";
 
 export function SignMessage(){
     const { publicKey , signMessage} = useWallet();
+    const [message, setMessage] = useState("");
+
 
     async function sign(){
         if(!publicKey){
@@ -14,7 +17,10 @@ export function SignMessage(){
             alert("Wallet does not supports message signing")
         }
 
-        const message = document.getElementById("inputEl").value;
+        if (!message || message.trim() === "") {
+            alert("Enter a message");
+            return;
+        }
 
         if(!message){
             alert("Enter Message");
@@ -33,7 +39,7 @@ export function SignMessage(){
     return (
             <div style={{marginTop: "20px",textAlign:"center"}}>
                 <h2 style={{ color:"#D1D1E0", fontSize:"35px",textDecoration:"underline"}}>Sign Message</h2>
-                <input id="inputEl" type="text" placeholder="Message" style={{padding:"10px",marginTop:"5px",backgroundColor:"#1c1c1c",width:"200px",borderRadius:"8px",border:"1px solid #4f8bff",color:"#ffffff", marginBottom:"10px"}}/>
+                <input value={message} onChange={(e) => setMessage(e.target.value)} id="inputEl" type="text" placeholder="Message" style={{padding:"10px",marginTop:"5px",backgroundColor:"#1c1c1c",width:"200px",borderRadius:"8px",border:"1px solid #4f8bff",color:"#ffffff", marginBottom:"10px"}}/>
                 <br />
                 <button id="buttonEl" onClick={sign} style={{color:"#ffffffff",marginTop:"5px",width:"100px",height:"50px",fontWeight:"bold",fontSize:"17px",borderRadius:"7px",background: "linear-gradient(135deg, #9945FF, #14F195)"}}>Sign Message</button>
             </div>
--- src/transaction.jsx
+++ src/transaction.jsx
@@ -1,4 +1,6 @@
 import { useWallet , useConnection } from "@solana/wallet-adapter-react";
+import { useState } from "react";
+
 import { 
     LAMPORTS_PER_SOL,
     Transaction,
@@ -10,16 +12,22 @@ export function SendTokens(){
     const wallet = useWallet();
     const {connection} = useConnection();
 
+    const [receiver, setReceiver] = useState("");
+    const [amount, setAmount] = useState("");
+
     async function Send(){
         if(!wallet.publicKey){
             alert("Connect Wallet !");
             return;
         }
-        const receiver = document.getElementById("receiver").value;
-        const amount = document.getElementById("amount").value;
         
         if(!receiver){
             alert("Enter Address");
+            return;
+        }
+
+         if (!amount || isNaN(Number(amount))) {
+            alert("Enter a valid number");
             return;
         }
 
@@ -34,16 +42,16 @@ export function SendTokens(){
 
         await wallet.sendTransaction(transaction, connection);
 
-        alert("Sent " + amount + "SOL to " + receiver)
+        alert("Sent " + amount + " SOL to " + receiver)
     }
 
     return (
         <div style={{marginRight:"500px",textAlign:"center"}}>
             <br />
             <h2 style={{color:"#D1D1E0", fontSize:"35px",textDecoration:"underline"}}>Sign Transaction</h2>
-            <input style={{padding:"10px",backgroundColor:"#1c1c1c",width:"200px",borderRadius:"8px",border:"1px solid #4f8bff",color:"#ffffff", marginBottom:"10px"}} id="receiver" type="text" placeholder="To" />
+            <input value={receiver} onChange={(e) => setReceiver(e.target.value)} style={{padding:"10px",backgroundColor:"#1c1c1c",width:"200px",borderRadius:"8px",border:"1px solid #4f8bff",color:"#ffffff", marginBottom:"10px"}} id="receiver" type="text" placeholder="To" />
             <br />
-            <input style={{padding:"10px",marginTop:"5px",backgroundColor:"#1c1c1c",width:"200px",borderRadius:"8px",border:"1px solid #4f8bff",color:"#ffffff", marginBottom:"10px"}} id="amount" type="text" placeholder="Amount" />
+            <input value={amount} onChange={(e) => setAmount(e.target.value)} style={{padding:"10px",marginTop:"5px",backgroundColor:"#1c1c1c",width:"200px",borderRadius:"8px",border:"1px solid #4f8bff",color:"#ffffff", marginBottom:"10px"}} id="amount" type="text" placeholder="Amount" />
             <br />
             <button style={{color:"#ffffffff",marginTop:"5px",width:"80px",height:"30px",fontWeight:"bold",fontSize:"17px",background: "linear-gradient(135deg, #9945FF, #14F195)",borderRadius:"7px"}} onClick={Send}>Send</button>
         </div>
